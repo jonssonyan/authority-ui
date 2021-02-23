b5f1c677d99f731dcbcbc1cf096020687cd67ba2
jonssonyan <yz808@outlook.com>
2021-02-24 01:07:59 +0800

    first commit

--- src/main.js
+++ src/main.js
@@ -1,12 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
 import ElementUI, {Message, MessageBox} from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
 // 导入全局样式表
 import './assets/css/global.css'
-import * as echarts from 'echarts'
 
 
 Vue.use(ElementUI);
@@ -17,11 +15,8 @@ Vue.config.productionTip = false;
 Vue.prototype.$message = Message;
 // confirm
 Vue.prototype.$confirm = MessageBox.confirm;
-// echarts
-Vue.prototype.$echarts = echarts;
 
 new Vue({
     router,
-    store,
     render: h => h(App)
 }).$mount('#app');
