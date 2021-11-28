5b57fb3cf14c2f7a574b11ae6478d42fd32468d2
jonssonyan <yz808@outlook.com>
2021-11-28 19:01:45 +0800

    fix: 重构

--- src/views/power/index.vue
+++ src/views/power/index.vue
@@ -11,8 +11,8 @@
       <el-row :gutter="20">
         <!--搜索与添加区域-->
         <el-col :span="6">
-          <el-input placeholder="请输入权限名称" v-model="queryInfo.name" clearable @clear="getRightList()">
-            <el-button slot="append" icon="el-icon-search" @click="getRightList()"></el-button>
+          <el-input placeholder="请输入权限名称" v-model="queryInfo.name" clearable @clear="getPowerList()">
+            <el-button slot="append" icon="el-icon-search" @click="getPowerList()"></el-button>
           </el-input>
         </el-col>
         <!--添加区域-->
@@ -21,7 +21,7 @@
         </el-col>
       </el-row>
       <!--权限列表区域-->
-      <el-table :data="rightList.records" border stripe>
+      <el-table :data="powerList.records" border stripe>
         <el-table-column label="#" type="index"></el-table-column>
         <el-table-column label="角色" prop="role.name"></el-table-column>
         <el-table-column label="权限描述" prop="permission.description"></el-table-column>
@@ -59,7 +59,7 @@
           :page-sizes="[10, 20, 30, 40]"
           :page-size="queryInfo.pageSize"
           layout="total, sizes, prev, pager, next, jumper"
-          :total="rightList.total">
+          :total="powerList.total">
       </el-pagination>
     </el-card>
     <!--添加权限的对话框-->
@@ -69,9 +69,9 @@
         width="50%"
         @close="addDialogClosed">
       <!--内容主体区域-->
-      <el-form :model="addRightForm" :rules="addRightRules" ref="addRightRef" label-width="70px">
-        <el-form-item label="角色" prop="role">
-          <el-select filterable v-model="addRightForm.roleId" placeholder="请选择">
+      <el-form :model="addPowerForm" :rules="powerRules" ref="powerRef" label-width="70px">
+        <el-form-item label="角色" prop="roleId">
+          <el-select filterable v-model="addPowerForm.roleId" placeholder="请选择">
             <el-option v-for="item in roleList"
                        :key="item.id"
                        :value="item.id"
@@ -79,8 +79,8 @@
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="权限" prop="permission">
-          <el-select filterable v-model="addRightForm.permissionId" placeholder="请选择">
+        <el-form-item label="权限" prop="permissionId">
+          <el-select filterable v-model="addPowerForm.permissionId" placeholder="请选择">
             <el-option v-for="item in permissionList"
                        :key="item.id"
                        :value="item.id"
@@ -89,7 +89,7 @@
           </el-select>
         </el-form-item>
         <el-form-item label="是否生效" prop="state">
-          <el-select v-model="addRightForm.state" placeholder="请选择">
+          <el-select v-model="addPowerForm.state" placeholder="请选择">
             <el-option v-for="(item,index) in options"
                        :key="index"
                        :value="item.value"
@@ -100,7 +100,7 @@
       </el-form>
       <span slot="footer" class="dialog-footer">
     <el-button @click="addDialogVisible = false">取 消</el-button>
-    <el-button type="primary" @click="addRight">确 定</el-button>
+    <el-button type="primary" @click="addPower">确 定</el-button>
   </span>
     </el-dialog>
     <!--修改权限的对话框-->
@@ -109,8 +109,8 @@
         :visible.sync="editDialogVisible"
         width="50%">
       <!--内容主体区域-->
-      <el-form :model="editRoleForm" :rules="addRightRules" ref="addRightRef" label-width="70px">
-        <el-form-item label="角色" prop="role">
+      <el-form :model="editRoleForm" :rules="powerRules" ref="powerRef" label-width="70px">
+        <el-form-item label="角色" prop="roleId">
           <el-select filterable v-model="editRoleForm.roleId" placeholder="请选择">
             <el-option v-for="item in roleList"
                        :key="item.id"
@@ -119,7 +119,7 @@
             </el-option>
           </el-select>
         </el-form-item>
-        <el-form-item filterable label="权限" prop="permission">
+        <el-form-item filterable label="权限" prop="permissionId">
           <el-select v-model="editRoleForm.permissionId" placeholder="请选择">
             <el-option v-for="item in permissionList"
                        :key="item.id"
@@ -159,7 +159,7 @@ export default {
         pageNum: 1,
         pageSize: 10
       },
-      rightList: {
+      powerList: {
         records: [],
         total: 0
       },
@@ -168,7 +168,7 @@ export default {
       // 控制修改权限对话框的显示与隐藏
       editDialogVisible: false,
       // 添加的权限对象
-      addRightForm: {
+      addPowerForm: {
         roleId: null,
         permissionId: null,
         state: 1
@@ -179,11 +179,11 @@ export default {
         permissionId: null
       },
       // 添加权限的验证规则
-      addRightRules: {
-        role: [
+      powerRules: {
+        roleId: [
           {required: true, message: '请输入角色', trigger: 'blur'}
         ],
-        permission: [
+        permissionId: [
           {required: true, message: '请输入权限描述', trigger: 'blur'},
         ]
       },
@@ -196,24 +196,24 @@ export default {
     }
   },
   created() {
-    this.getRightList();
+    this.getPowerList();
   },
   methods: {
-    getRightList() {
+    getPowerList() {
       selectPage(this.queryInfo).then(res => {
-        this.rightList.total = res.data.total;
-        this.rightList.records = res.data.records;
+        this.powerList.total = res.data.total;
+        this.powerList.records = res.data.records;
       });
     },
     // 监听pageSize改变的事件
     handleSizeChange(newSize) {
       this.queryInfo.pageSize = newSize;
-      this.getRightList()
+      this.getPowerList()
     },
     // 监听pageNum改变的事件
     handleCurrentChange(newPage) {
       this.queryInfo.pageNum = newPage;
-      this.getRightList()
+      this.getPowerList()
     },
     stateChange(info) {
       saveOrUpdate(info).then(() => {
@@ -222,17 +222,17 @@ export default {
     },
     // 监听添加权限对话框的关闭事件
     addDialogClosed() {
-      this.$refs['addRightRef'].resetFields();
+      this.$refs['powerRef'].resetFields();
     },
     // 点击按钮添加新权限
-    addRight() {
-      this.$refs.addRightRef.validate(async valid => {
+    addPower() {
+      this.$refs.powerRef.validate(async valid => {
         if (!valid) return;
         // 发起添加权限的网络请求
-        saveOrUpdate(this.addRightForm).then(() => {
+        saveOrUpdate(this.addPowerForm).then(() => {
           this.$message.success("添加权限成功");
           // 重新获取权限列表
-          this.getRightList();
+          this.getPowerList();
           // 隐藏添加权限对话框
           this.addDialogVisible = false;
         });
@@ -243,40 +243,40 @@ export default {
       getById({id: role.id}).then(res => {
         this.editRoleForm = res.data;
       });
-      select(this.addRightForm).then(res => {
+      select(this.addPowerForm).then(res => {
         this.roleList = res.data;
       });
-      selectPermission(this.addRightForm).then(res => {
+      selectPermission(this.addPowerForm).then(res => {
         this.permissionList = res.data;
       });
       this.editDialogVisible = true;
     },
     // 展示添加权限的对话框
     showAddDialog() {
-      select(this.addRightForm).then(res => {
+      select(this.addPowerForm).then(res => {
         this.roleList = res.data;
       });
-      selectPermission(this.addRightForm).then(res => {
+      selectPermission(this.addPowerForm).then(res => {
         this.permissionList = res.data;
       });
       this.addDialogVisible = true;
     },
     // 修改权限信息并提交
     editRoleInfo() {
-      this.$refs.addRightRef.validate(async valid => {
+      this.$refs.powerRef.validate(async valid => {
         if (!valid) return;
         // 发起修改权限的网络请求
         saveOrUpdate(this.editRoleForm).then(() => {
           this.$message.success("修改权限成功");
           // 重新获取权限列表
-          this.getRightList();
+          this.getPowerList();
           // 隐藏添加权限对话框
           this.editDialogVisible = false;
         });
       })
     },
     // 根据id删除权限信息
-    removeById(right) {
+    removeById(power) {
       // 弹框询问用户是否删除权限
       this.$confirm('此操作将永久删除该权限, 是否继续?', '提示', {
         confirmButtonText: '确定',
@@ -284,10 +284,10 @@ export default {
         type: 'warning'
       }).then(() => {
         // 删除权限
-        removeById({id: right.id}).then(() => {
+        removeById({id: power.id}).then(() => {
           this.$message.success("删除权限成功");
           // 重新获取权限列表
-          this.getRightList();
+          this.getPowerList();
           this.$message({
             type: 'success',
             message: '删除成功!'
