d37be79a751d7414800fc78b8f0a738216608b18
jonssonyan <yz808@outlook.com>
2021-11-28 20:07:11 +0800

    fix: 重构

--- src/router/index.js
+++ src/router/index.js
@@ -20,7 +20,6 @@ const routes = [
     {
         path: '/layout',
         component: () => import('../layout'),
-        redirect: '/user',
         children: [
             {
                 path: '/user',
