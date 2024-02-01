84d23bcf8529fb8d550cd145e8b082f37c3266d2
jonssonyan <yz808@outlook.com>
2024-02-01 16:54:48 +0800

    fix: 格式化

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -18,6 +18,7 @@
           <el-input
             prefix-icon="el-icon-user"
             placeholder="请输入用户名"
+            @keyup.enter.native="login"
             v-model="loginForm.username"
             type="text"
           ></el-input>
@@ -34,9 +35,7 @@
         </el-form-item>
         <!--按钮区域-->
         <el-form-item class="btns">
-          <el-button type="primary" @click="login" @keyup.enter="login"
-            >登录
-          </el-button>
+          <el-button type="primary" @click="login">登录 </el-button>
           <el-button type="primary" @click="register">注册</el-button>
           <el-button type="info" @click="resetLoginForm">重置</el-button>
         </el-form-item>
